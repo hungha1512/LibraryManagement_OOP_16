32232f1f18548ae2f6d295053907ba9c665300fd
Hà Quang Hưng <hungha15123@gmail.com>
2024-11-11 13:49:19 +0700

    fix: change the source of FXMLLoader

--- src/main/java/com/hunghq/librarymanagement/LibraryApplication.java
+++ src/main/java/com/hunghq/librarymanagement/LibraryApplication.java
@@ -10,8 +10,11 @@ import javafx.stage.Stage;
 public class LibraryApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("homepage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(LibraryApplication.class
+                .getResource("/com/hunghq/librarymanagement/View/Login.fxml")
+        );
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
